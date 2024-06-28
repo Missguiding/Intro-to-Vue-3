3e19a3a5e3354fb4dd26bd40d9769e949d0753b5
Missguiding <lindsey141080@hotmail.com>
2024-06-28 15:17:01 +0200

    Ex + solution L4

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            inventory: 0,
+            //Code challenge L4
+            onSale: false
         }
     }
 })
