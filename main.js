6cbb1afabad7d0d13b6fa3194a2beb2165f8f594
Missguiding <lindsey141080@hotmail.com>
2024-06-28 14:24:09 +0200

    Ex + Code Challenge

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Test'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg', // target één van de images(toegevoegd aan de assets folder) door het path mee te geven
+
+            //Code Challenge L3            
+            url: 'https://www.prosafco.be/'           
         }
     }
 })
