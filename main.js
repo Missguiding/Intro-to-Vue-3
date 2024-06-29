4972b64fc547a5b872905ca45828ba9ad8edee53
Missguiding <lindsey141080@hotmail.com>
2024-06-29 12:02:41 +0200

    L6 - events: v-on / v-mouseover / methods

--- main.js
+++ main.js
@@ -11,5 +11,13 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1 // refereert naar cart in data hierboven
+        },
+        updateImage(variantImage){ // parameter variant.image - zie index parameter variant.image
+            this.image = variantImage // in de methode zeggen we deze imaga is gelijk aan de doorgegeven (parameter) image
+        }
     }
 })
