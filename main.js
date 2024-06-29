79d2a69f3769c946ac2f6498b0dcc1debce528e6
Missguiding <lindsey141080@hotmail.com>
2024-06-29 10:20:19 +0200

    L5- Lists: ul/li/v-for/ :key

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            // dit is de collection waar over zal geitireerd worden in de v-for
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            // deze collectie bevat een id en een kleur per product
+            variants:[
+                {id:2234, color: 'green'},
+                {id:2235, color: 'blue'}
+            ],
+            
+            size: ['S,M,L,XL']
         }
     }
 })
