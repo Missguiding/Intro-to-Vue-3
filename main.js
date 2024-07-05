68a3110c4427dc13aa28d494f06f79b6228abb84
Missguiding <lindsey141080@hotmail.com>
2024-07-05 14:26:57 +0200

    Ex + Code Challenge

--- main.js
+++ main.js
@@ -3,22 +3,43 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            brand: 'Vue Mastery', // toegevoegd voor de oefening
+          // image: './assets/images/socks_blue.jpg', // deze verwijderen we en we maken een nieuwe property, selectedVariant
+            selectedVariant: 0, // 0 omdat we het updaten met de index waar we over hoveren
+           // inStock: false, inStock mag ook weg omdat we dat samen met de image in de computed propery steken
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ],
+            onSale: true,
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) { // deze zullen we wijzigen naar updateVariant met als parameter 'index'
+            this.selectedVariant = index // selectedVariant zetten we gelijk aan de index
+        }
+    },
+    computed:{
+        title(){
+            return this.brand + " " + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image // op zoek naar de variant;
+           //binne de array[]  0=groen, 1=blauw en de .image is om de correcte afbeelding weer te geven
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity // op zoek naar de variant;
+           //binnen de array[]  0=groen, 1=blauw en de .image is om de correcte afbeelding weer te geven
+        },
+        // Code Challenge L8
+        sale(){
+            if(this.onSale){
+                 return this.brand + ' ' + this.product + ' is on sale.'
+            }            
         }
     }
 })
